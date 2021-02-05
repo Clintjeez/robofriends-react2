da7ffc466fa8de4ae29f322a5800ece80ae9f397
Clintjeez <Clintjeez>
2021-02-05 09:31:17 +0000

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Lesan Graham",
+    username: "Lagram",
+    email: "leham@mail.com",
+  },
+  {
+    id: 2,
+    name: "Belli Clanp",
+    username: "Belclan",
+    email: "belclan@mail.com",
+  },
+  {
+    id: 3,
+    name: "Dan Groove",
+    username: "Dangro",
+    email: "dangro@mail.com",
+  },
+  {
+    id: 4,
+    name: "Lisa plex",
+    username: "Liplex",
+    email: "liplex@mail.com",
+  },
+  {
+    id: 5,
+    name: "Ben Okoro",
+    username: "Benkoro",
+    email: "benkoro@mail.com",
+  },
+  {
+    id: 6,
+    name: "Lesan Graham",
+    username: "Lagram",
+    email: "leham@mail.com",
+  },
+  {
+    id: 7,
+    name: "Lesan Graham",
+    username: "Lagram",
+    email: "leham@mail.com",
+  },
+  {
+    id: 8,
+    name: "Lesan Graham",
+    username: "Lagram",
+    email: "leham@mail.com",
+  },
+  {
+    id: 9,
+    name: "Lesan Graham",
+    username: "Lagram",
+    email: "leham@mail.com",
+  },
+  {
+    id: 10,
+    name: "Lesan Graham",
+    username: "Lagram",
+    email: "leham@mail.com",
+  },
+];
